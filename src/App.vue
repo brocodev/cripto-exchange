e1fdc194a111f27989c7198c31bbc8f600462c46
kevinmdezhdez <kevinmdezhdez@gmail.com>
2019-12-12 06:29:52 -0600

    Primer commit luego de estudiar las bases de Vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <div id="app">
+  <!--En esta parte dentro del App.vue se genera el template que se rendiraza en "main.js"  -->
+  <div id="app" class="bg-red-600">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!--El componente "HelloWorld" se importa desde el archivo "HelloWord.vue"-->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld.vue"; //<--Importando el componente "HelloWorld"
 
 export default {
-  name: "app",
+  //<--Este export es parte de los modulos de EcmaScript
+  name: "app", //<--La propiedad "name" es opcional y nos permite asiganarle un nombre al componente, lo cual es util al momente de hacer debug
   components: {
     HelloWorld
-  }
+  } //<--La propiedad components nos permite atachar cualquier componente para poder usarlo dentro de la parte de "<template>"
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"; //<--Importa la libreria de Vue (ESLint 2015)
+import App from "./App.vue"; //<--Importa el archivo de componentes App.Vue
+import "@/assets/css/tailwind.css";
+//"@" hace referencia a la carpeta "src" de nuestro proyecto
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false; //<--Esta configuracion sirve para indicar si queremos tener tips de produccion
 
+//Este siguiente bloque de codigo lo que hace es declarar una instancia
+//de Vue usando la funcion "render"
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+    render: h => h(App)
+}).$mount("#app"); //<-- La función "mount" es el quivalente al atributo "el"
